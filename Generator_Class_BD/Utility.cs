44549462bb76ad74e9f4381ce4cc6e6e1018dfa5
Jeitson <jeitson@gmail.com>
2025-04-28 17:52:18 -0500

    pr6

--- Generator_Class_BD/Utility.cs
+++ Generator_Class_BD/Utility.cs
@@ -21,7 +21,7 @@ namespace Generator_Class_BD
             StreamWriter writer = File.CreateText(fileName);
             writer.WriteLine(contentFile.ToString());
             writer.Close();
-            Console.WriteLine("iniciado para prueba numero 3, se debe revisar webhook porque no funciona asdjasbdks");
+            Console.WriteLine("iniciado para prueba numero 3, se debe revisar webhook porque no funciona nuevos eventos");
         }
     }
 }
