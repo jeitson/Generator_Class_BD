b7c3c661168f2a9fc9f900a9a94ee8a7532f91ef
Jeitson <jeitson@gmail.com>
2025-04-28 20:44:11 -0500

    pr8

--- Generator_Class_BD/Utility.cs
+++ Generator_Class_BD/Utility.cs
@@ -21,7 +21,10 @@ namespace Generator_Class_BD
             StreamWriter writer = File.CreateText(fileName);
             writer.WriteLine(contentFile.ToString());
             writer.Close();
-            Console.WriteLine("iniciado jnaskfnafjnlfnla");
+            Console.WriteLine("iniciado 1");
+            Console.WriteLine("iniciado 2");
+            Console.WriteLine("iniciado 3");
+            Console.WriteLine("iniciado 4");
         }
     }
 }
